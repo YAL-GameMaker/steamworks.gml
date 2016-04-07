95fcd05529a972287b3f9ea551c2fdf7976b8e9f
YellowAfterlife <yellowafterlife@ymail.com>
2016-04-07 16:07:42 +0300

    Updated the Linux build script and instructions.

--- build_linux.sh
+++ build_linux.sh
@@ -1,2 +1,9 @@
-gcc -O3 -fPIC -Wall -g -c -o Steamworks.gml/Steamworks.gml.o Steamworks.gml/Steamworks.gml.cpp
-gcc Steamworks.gml/Steamworks.gml.o -shared -o Steamworks.gmx/extensions/Steamworks.gml/Steamworks.gml.so -Wl,-rpath,assets/ -LSteamworks/redistributable_bin/linux32 -lsteam_api
+#!/bin/sh
+ARCH=`uname -m`
+if [ "$ARCH" = "x86" ]; then
+	REDIST="linux32"
+else
+	REDIST="linux64"
+fi
+gcc -O3 -fPIC -g -c -o Steamworks.gml/Steamworks.gml.o Steamworks.gml/Steamworks.gml.cpp
+gcc Steamworks.gml/Steamworks.gml.o -shared -o Steamworks.gmx/extensions/Steamworks.gml/Steamworks.gml.so -Wl,-rpath,assets/ -LSteamworks/redistributable_bin/$REDIST -lsteam_api
