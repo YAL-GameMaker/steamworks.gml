2ec4e97b13f445a0df72469786250f4d8faf70cb
YellowAfterlife <yellowafterlife@ymail.com>
2016-07-08 20:33:43 +0300

    OSX extension now references libsteam_api.dylib in @loader_path as it should (affects OSX YYC).

--- build_osx.sh
+++ build_osx.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 g++ -m32 -O3 -fPIC -g -c -o Steamworks.gml/Steamworks.gml.o Steamworks.gml/Steamworks.gml.cpp
 g++ -m32 Steamworks.gml/Steamworks.gml.o -shared -o Steamworks.gmx/extensions/Steamworks.gml/Steamworks.gml.dylib -Wl,-rpath,assets/ -LSteamworks/redistributable_bin/osx32 -lsteam_api
+install_name_tool -change libssteam_api.dylib @loader_path/libsteam_api.dylib Steamworks.gmx/extensions/Steamworks.gml/Steamworks.gml.dylib
