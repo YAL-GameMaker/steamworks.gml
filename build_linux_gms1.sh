7068a782ad1137f1bf7e2565317b3054ebd06ad4
Anton Terisback <terisbackno@gmail.com>
2021-01-02 18:11:49 +0500

    release path

--- build_linux_gms1.sh
+++ build_linux_gms1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 g++ -m32 Steamworks.gml/steam_*.cpp \
-    -std=c++11 -shared -o Steamworks.gmx/extensions/Steamworks.gml/Steamworks.gml.so \
+    -std=c++11 -shared -o Steamworks.gml.so \
     -Wl,-rpath,assets/ -LSteamworks/redistributable_bin/linux32 -lsteam_api
--- build_osx_gms2.sh
+++ build_osx_gms2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 g++ Steamworks.gml/steam_*.cpp \
-    -std=c++11 -shared -o Steamworks_yy/extensions/Steamworks_gml/Steamworks.gml.dylib \
+    -std=c++11 -shared -o Steamworks.gml.dylib \
     -Wl,-rpath,assets/ -LSteamworks/redistributable_bin/osx32 -lsteam_api -fPIC
 # VM currently needs native extension to be in @loader_path:
-cp Steamworks_yy/extensions/Steamworks_gml/Steamworks.gml.dylib Steamworks_yy/extensions/Steamworks_gml/Steamworks.gml.vm.dylib
+cp Steamworks.gml.dylib Steamworks.gml.vm.dylib
 # YYC, on other hand, needs extensions to be in @executable_path:
-install_name_tool -change @loader_path/libsteam_api.dylib @executable_path/libsteam_api.dylib Steamworks_yy/extensions/Steamworks_gml/Steamworks.gml.dylib
+install_name_tool -change @loader_path/libsteam_api.dylib @executable_path/libsteam_api.dylib Steamworks.gml.dylib
