22b8d6ca65921e2fbf1e221ec5d8b7c1abc25c73
Anton Terisback <terisbackno@gmail.com>
2021-01-02 09:33:53 +0500

    fixes

--- post_build.bat
+++ post_build.bat
@@ -1,3 +1,4 @@
+mkdir "Steamworks.gmx\extensions\Steamworks.gml"
 copy /y "Steamworks_yy\extensions\Steamworks_gml\Steamworks.gml.gml" "Steamworks.gmx\extensions\Steamworks.gml\Steamworks.gml.gml"
 
 type "Steamworks.gml\steam_*.h" "Steamworks.gml\steam_*.cpp" > "Steamworks_yy\extensions\Steamworks_gml\Steamworks.gml.cpp"
@@ -11,6 +12,6 @@ copy /y "export\steamworks.gml.html" "Steamworks.gmx\datafiles\steamworks.gml.ht
 copy /y "export\steamworks.gml.html" "Steamworks_yy\datafiles\steamworks.gml.html"
 copy /y "export\steamworks.gml.html" "Steamworks_23\datafiles\steamworks.gml.html"
 
-GmxGen "Steamworks.gmx\extensions\Steamworks.gml.extension.gmx"
-GmxGen "Steamworks_yy\extensions\Steamworks_gml\Steamworks_gml.yy"
-GmxGen "Steamworks_23\extensions\Steamworks_gml\Steamworks_gml.yy"
+.\GmxGen.exe "Steamworks.gmx\extensions\Steamworks.gml.extension.gmx"
+.\GmxGen.exe "Steamworks_23\extensions\Steamworks_gml\Steamworks_gml.yy"
+.\GmxGen.exe "Steamworks_yy\extensions\Steamworks_gml\Steamworks_gml.yy"
