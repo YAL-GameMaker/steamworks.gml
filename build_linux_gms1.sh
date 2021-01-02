1638181580269f0293446c96305715fa81a91837
Anton Terisback <terisbackno@gmail.com>
2021-01-02 18:05:18 +0500

    unix is there (almost)

--- build_linux_gms1.sh
+++ build_linux_gms1.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-g++ -m32 Steamworks.gml/steam_*.cpp -std=c++11 -shared -o Steamworks.gmx/extensions/Steamworks.gml/Steamworks.gml.so -Wl,-rpath,assets/ -LSteamworks/redistributable_bin/linux32 -lsteam_api
+g++ -m32 Steamworks.gml/steam_*.cpp \
+    -std=c++11 -shared -o Steamworks.gmx/extensions/Steamworks.gml/Steamworks.gml.so \
+    -Wl,-rpath,assets/ -LSteamworks/redistributable_bin/linux32 -lsteam_api
--- build_linux_gms2.sh
+++ build_linux_gms2.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-g++ -m64 Steamworks.gml/steam_*.cpp -std=c++11 -shared -o Steamworks.gmx/extensions/Steamworks.gml/Steamworks.gml.so -Wl,-rpath,assets/ -LSteamworks/redistributable_bin/linux32 -lsteam_api
+g++ -m64 Steamworks.gml/steam_*.cpp \
+    -std=c++11 -shared -o Steamworks.gmx/extensions/Steamworks.gml/Steamworks.gml.so \
+    -Wl,-rpath,assets/ -LSteamworks/redistributable_bin/linux64 -lsteam_api -fPIC
