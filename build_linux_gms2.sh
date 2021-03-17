1ffedc8c77303a514eec05ba05de688585c1f748
YellowAfterlife <yellowafterlife@hotmail.com>
2021-03-17 23:15:31 +0200

    And point at header directory to be sure

--- build_linux_gms2.sh
+++ build_linux_gms2.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-g++ -m64 Steamworks.gml/steam_*.cpp \
-    -std=c++11 -shared -o Steamworks.gml.so \
-    -Wl,-rpath,assets/ -Lsteamworks_sdk_142/redistributable_bin/linux64 -lsteam_api -fPIC
+g++ -m64 \
+    -std=c++11 \
+    Steamworks.gml/steam_*.cpp \
+    -Wl,-rpath,assets/ -fPIC \
+    -Lsteamworks_sdk_142/redistributable_bin/linux64 \
+    -lsteam_api \
+    -Isteamworks_sdk_142/public \
+    -shared -o Steamworks.gml.so
--- build_osx_gms2.sh
+++ build_osx_gms2.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-g++ Steamworks.gml/steam_*.cpp \
-    -std=c++11 -shared -o Steamworks.gml.dylib \
-    -Wl,-rpath,assets/ -Lsteamworks_sdk_142/redistributable_bin/osx32 -lsteam_api -fPIC
+g++ \
+    -std=c++11 \
+    Steamworks.gml/steam_*.cpp \
+    -Wl,-rpath,assets/ -fPIC \
+    -Lsteamworks_sdk_142/redistributable_bin/osx32 \
+    -lsteam_api \
+    -Isteamworks_sdk_142/public \
+    -shared -o Steamworks.gml.dylib
 # VM currently needs native extension to be in @loader_path:
 cp Steamworks.gml.dylib Steamworks.gml.vm.dylib
 # YYC, on other hand, needs extensions to be in @executable_path:
